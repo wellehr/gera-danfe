e7eb5c4a68cfc115088be96fcd652ae76087ecc2
Wellisson Ribeiro <welleh10@gmail.com>
2020-01-30 17:13:32 -0300

    'cdns'

--- index.php
+++ index.php
@@ -14,8 +14,8 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Gerador de Danfe Online</title>
 	<link rel="icon" href="images/favicon.png">
-	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
-	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </head>
 <body class="body" style="padding: 0; margin: 0; width: 100%">
 	<div class="container" style="padding-top: 5%">
@@ -28,7 +28,7 @@
 			</div>
 		</div>
 		<div class="row">
-			<div class="col-md-8 col-md-offset-4">
+			<div class="col-md-6 col-md-offset-3">
 				<div class="panel-body">
 					<ul class="nav nav-tabs">
                         <li class="active">
